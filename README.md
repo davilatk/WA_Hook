0e6ae3c78fccf8af7f7e328a7c45a73ad7a7b267
davilatk <diego-pem@outlook.com>
2022-05-21 12:37:43 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# WhatsApp Business Platform Quick Start
+
+Welcome to your first step toward building awesome WhatsApp apps!
+
+This project contains the code for a simple webhook you can use to get started using the WhatsApp Platform.
+
+The code here mirrors what is in our [webhook set up guide](https://developers.facebook.com/docs/messenger-platform/getting-started/webhook-setup), and may be used as the starting point for doing the [WhatsApp Business Platform quick start tutorial](https://developers.facebook.com/docs/whatsapp/getting-started/signing-up/).
+
+## Additional Resources
+
+Interested in learning more about the WhatsApp Platform? Check out these resources:
+
+- [**Webhook set up guide**](https://developers.facebook.com/docs/whatsapp/getting-started/signing-up/#configure-webhooks): The walkthrough for the code in this project.
+
+- [**Quick start tutorial**](https://developers.facebook.com/docs/whatsapp/getting-started/signing-up/): Build your first app by remixing this project and following our quick start tutorial.
+
+- [**Documentation**](https://developers.facebook.com/docs/whatsapp/)
---
+++ app.js
@@ -0,0 +1,61 @@
+/*
+ * Starter Project for WhatsApp Webhook Tutorial
+ *
+ * Remix this as the starting point for following the WhatsApp Getting Started tutorial
+ *
+ * https://developers.facebook.com/docs/whatsapp/getting-started/signing-up/
+ *
+ */
+
+"use strict";
+
+// Imports dependencies and set up http server
+const request = require("request"),
+  express = require("express"),
+  body_parser = require("body-parser"),
+  app = express().use(body_parser.json()); // creates express http server
+
+// Sets server port and logs message on success
+app.listen(3000, () => console.log("webhook is listening"));
+
+// Accepts POST requests at /webhook endpoint
+app.post("/webhook", (req, res) => {
+  // Parse the request body from the POST
+  let body = req.body;
+
+  // Check the Incoming webhook message
+  console.log("Incoming webhook: " + JSON.stringify(req.body));
+
+  // Validate the webhook
+  if (req.body.object) {
+    res.sendStatus(200);
+  } else {
+    // Return a '404 Not Found' if event is not from a whatsApp API
+    res.sendStatus(404);
+  }
+});
+
+// Accepts GET requests at the /webhook endpoint. You need this URL to setup webhook initially.
+app.get("/webhook", (req, res) => {
+  /** UPDATE YOUR VERIFY TOKEN
+  This will be the Verify Token value when you set up webhook**/
+  const VERIFY_TOKEN = "GCMPWEBTOCKEN";
+
+  // Parse params from the webhook verification request
+  let mode = req.query["hub.mode"];
+  let token = req.query["hub.verify_token"];
+  let challenge = req.query["hub.challenge"];
+
+  // Check if a token and mode were sent
+  if (mode && token) {
+    // Check the mode and token sent are correct
+    if (mode === "subscribe" && token === VERIFY_TOKEN) {
+      // Respond with 200 OK and challenge token from the request
+      console.log("WEBHOOK_VERIFIED");
+      res.status(200).send(challenge);
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);
+    }
+  }
+});
